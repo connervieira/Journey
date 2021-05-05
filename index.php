54cd50144d32e90da18be273c8e9ce96e82bfd66
Conner Vieira <connervieira@tutanota.com>
2021-05-05 11:20:59 -0400

    Created and started main page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Moop - Main Page</title>
+        <link href="./stylesheets/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Moop</h1>
+        <h3>A crowd-sourced road hazard finder</h3>
+        <a class="button" href="signup.php">Sign Up</a>
+        <a class="button" href="signin.php">Sign In</a>
+    </body>
+</html>
